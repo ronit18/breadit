623969e02c8246f1fb57a6350f459db4ea078bcb
ronit18 <ronitgandhi96@gmail.com>
2023-06-30 16:41:40 +0530

    Added the Navbar with icon and sign-in button.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from '@/components/Navbar'
 import { cn } from '@/lib/utils'
 import '@/styles/globals.css'
 import { Inter } from 'next/font/google'
@@ -15,8 +16,19 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html className={cn('bg-white text-slate-900 antialiased light')} lang="en">
-      <body>{children}</body>
+    <html
+      className={cn(
+        'bg-white text-slate-900 antialiased light',
+        inter.className
+      )}
+      lang="en"
+    >
+      <body className="min-h-screen pt-12 bg-slate-50 antialiased">
+        <Navbar />
+        <div className="container max-w-7xl mx-auto h-full pt-12">
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
